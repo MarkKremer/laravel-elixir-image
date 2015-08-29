291599193433f5400aba69d986f5a6e8ac80d5d3
Mark Kremer <mail@markkremer.nl>
2015-08-29 23:59:20 +0200

    Bugfixes

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ var elixir = require('laravel-elixir'),
     path = require('path');
 
 module.exports = {
+    executables: {
+        convert: path.join(__dirname, 'imagemagick/convert')
+    },
     source: {
         path: path.join(elixir.config.assetsPath, 'img') // resources/assets/img
     },
